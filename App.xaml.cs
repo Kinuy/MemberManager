5b982e2c385da9eb01e0b5c06885b5c49a94595f
Tobias Weber <toby-weber@gmx.de>
2025-01-29 11:58:27 +0100

    add mainwindow

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,12 @@ namespace MemberManager
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 
 }
