74ebe047cd6b5e15b6cfe11b499c3d6a810d5ad3
Tobias Weber <toby-weber@gmx.de>
2025-01-29 17:22:10 +0100

    add ViewModels to bind to the properties

---
+++ ViewModel/MemberListingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberManager.ViewModel;
+
+public class MemberListingViewModel :ViewModelBase
+{
+    private readonly ObservableCollection<MemberListingItemViewModel> _memberListingItemViewModels;
+    public IEnumerable<MemberListingItemViewModel> MemberListingItemViewModels => _memberListingItemViewModels;
+
+    public MemberListingViewModel()
+    {
+        _memberListingItemViewModels = new ObservableCollection<MemberListingItemViewModel>();
+
+        _memberListingItemViewModels.Add(new MemberListingItemViewModel("Tobi"));
+        _memberListingItemViewModels.Add(new MemberListingItemViewModel("Steffi"));
+        _memberListingItemViewModels.Add(new MemberListingItemViewModel("Lucy"));
+    }
+}
