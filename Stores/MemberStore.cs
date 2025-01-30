10071d3b301feca203509116d2065e0d7fac3c05
Tobias Weber <toby-weber@gmx.de>
2025-01-30 18:25:33 +0100

    add member store

---
+++ Stores/MemberStore.cs
@@ -0,0 +1,24 @@
+﻿using MemberManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberManager.Stores;
+
+public class MemberStore
+{
+    public event Action<Member> MemberAdded;
+    public event Action<Member> MemberUpdated;
+
+    public async Task Add(Member member)
+    {
+        MemberAdded?.Invoke(member);
+    }
+
+    public async Task Update(Member member)
+    {
+        MemberUpdated?.Invoke(member);
+    }
+}
