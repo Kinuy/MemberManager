34050385573faf32c7bbbf921868cf80fe86c414
Tobias Weber <toby-weber@gmx.de>
2025-01-30 14:36:54 +0100

    add open command logic

---
+++ Commands/OpenAddMemberCommand.cs
@@ -0,0 +1,27 @@
+﻿using MemberManager.Stores;
+using MemberManager.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MemberManager.Commands;
+
+public class OpenAddMemberCommand : CommandBase
+{
+
+    private readonly ModalNavigationStore _modalNavigationStore;
+
+    public OpenAddMemberCommand(ModalNavigationStore modalNavigationStore)
+    {
+        _modalNavigationStore = modalNavigationStore;
+    }
+
+    public override void Execute(object? parameter)
+    {
+        AddMemberViewModel addMemberViewModel = new AddMemberViewModel(_modalNavigationStore);
+        _modalNavigationStore.CurrentViewModel = addMemberViewModel;
+    }
+}
