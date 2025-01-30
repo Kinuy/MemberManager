4f0c38cee3628b5e731588c67603282986dc4d74
Tobias Weber <toby-weber@gmx.de>
2025-01-30 18:17:32 +0100

    add asyn comand base to keep gui reactive

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MemberManager.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberManager.ViewModel;
+
+public class MainViewModel :ViewModelBase
+{
+    private readonly ModalNavigationStore _modalNavigationStore;
+
+    public ViewModelBase CurrentModalViewModel => _modalNavigationStore.CurrentViewModel;
+
+    public bool IsModalOpen => _modalNavigationStore.IsOpen;
+
+    public MemberManagerViewModel MemberManagerViewModel { get; }
+
+    public MainViewModel(ModalNavigationStore modalNavigationStore, MemberManagerViewModel memberManagerViewModel)
+    {
+        _modalNavigationStore = modalNavigationStore;
+        MemberManagerViewModel = memberManagerViewModel;
+
+        _modalNavigationStore.CurrentViewModelChanged += ModalNavigationStore_CurrentViewModelChanged;
+
+    }
+
+    protected override void Dispose()
+    {
+        _modalNavigationStore.CurrentViewModelChanged -= ModalNavigationStore_CurrentViewModelChanged;
+
+        base.Dispose();
+    }
+
+    private void ModalNavigationStore_CurrentViewModelChanged()
+    {
+        OnPropertyChanged(nameof(CurrentModalViewModel));
+        OnPropertyChanged(nameof(IsModalOpen));
+    }
+}
