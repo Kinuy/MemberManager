54b7720d93173704087c16fe8c963efa78eb876e
Tobias Weber <toby-weber@gmx.de>
2025-01-30 18:19:37 +0100

    add open command to add member

--- Commands/OpenAddMemberCommand.cs
+++ Commands/OpenAddMemberCommand.cs
@@ -11,17 +11,18 @@ namespace MemberManager.Commands;
 
 public class OpenAddMemberCommand : CommandBase
 {
-
+    private readonly MemberStore _memberStore;
     private readonly ModalNavigationStore _modalNavigationStore;
 
-    public OpenAddMemberCommand(ModalNavigationStore modalNavigationStore)
+    public OpenAddMemberCommand(MemberStore memberStore, ModalNavigationStore modalNavigationStore)
     {
+        _memberStore = memberStore;
         _modalNavigationStore = modalNavigationStore;
     }
 
     public override void Execute(object? parameter)
     {
-        AddMemberViewModel addMemberViewModel = new AddMemberViewModel(_modalNavigationStore);
+        AddMemberViewModel addMemberViewModel = new AddMemberViewModel(_memberStore,_modalNavigationStore);
         _modalNavigationStore.CurrentViewModel = addMemberViewModel;
     }
 }
